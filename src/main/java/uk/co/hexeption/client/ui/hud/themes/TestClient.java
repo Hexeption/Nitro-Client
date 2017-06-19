fb74a3d1e7f77325fcaad9ea132839e5ea2bfa35
Hexeption <minecraftfun201@gmail.com>
2017-06-19 12:43:29 +0100

    Updated Licenese
    Signed-off-by: Hexeption <minecraftfun201@gmail.com>

--- src/main/java/uk/co/hexeption/client/ui/hud/themes/TestClient.java
+++ src/main/java/uk/co/hexeption/client/ui/hud/themes/TestClient.java
@@ -1,18 +1,19 @@
 /*******************************************************************************
- *             DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
- *                     Version 2, December 2004
+ *     ITweaker-Client
+ *     Copyright (C) 2017  Hexeption (Keir Davis)
  *
- *  Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
  *
- *  Everyone is permitted to copy and distribute verbatim or modified
- *  copies of this license document, and changing it is allowed as long
- *  as the name is changed.
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
  *
- *             DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
- *    TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
- *
- *   0. You just DO WHAT THE FUCK YOU WANT TO.
- *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 
 package uk.co.hexeption.client.ui.hud.themes;
